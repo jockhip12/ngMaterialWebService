7f788f1e9f3b5ffc74124ba7bd19b1c822a1ca2a
Mohamed Anouar Abid <mabid@mabid-cpk.vm.vdi.s1.p.fti.net>
2018-04-19 18:47:43 +0200

    add content files and folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  myData: any;
+
+  constructor(private http:HttpClient) {
+    
+    this.http.get('https://jsonplaceholder.typicode.com/photos')
+      
+      
+      .subscribe(res => {this.myData = res; console.log(res);})
+      
+      
+
+  }  
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCardModule,
+     MatMenuModule,
+      MatToolbarModule, 
+      MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
